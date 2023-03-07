f8f8b2d2105ee878f3cebc7a14411b7c87707c26
tomas <A48286@alunos.isel.pt>
2023-03-07 16:34:24 +0000

    Adding comments

--- src/jogo/ambiente/Ambiente.java
+++ src/jogo/ambiente/Ambiente.java
@@ -1,39 +1,79 @@
 package jogo.ambiente;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Ambiente {
     /**
-     * HashMap criado para guardar a relação entre uma String e um Evento
-     * para facilitar a criação de eventos
+     * Atributos privados da classe Ambiente: eventos, evento e sc.
+     *
+     * O atributos eventos é um HashMap que associa as strings que podem
+     * ser introduzidas pelo utilizador a um evento.
+     *
+     * O atributo evento indica o evento mais recente que aconteceu no
+     * ambiente.
+     *
+     * O atributo sc é um scanner para permitir o utilizador interagir com
+     * o jogo através da consola.
      */
-    private HashMap<String, Evento> eventos = new HashMap<>();
+    private HashMap<String, Evento> eventos;
     private Evento evento;
+    private Scanner sc = new Scanner(System.in);
 
     /**
-     * Função que retorna o valor do atributo privado evento
-     * @return Evento
+     * Método construtor da classe.
+     *
+     * Faz a criação de um HashMap com os vários comandos (Strings) que
+     * o utilizador pode usar para escolher o evento (Evento).
      */
-    public Evento getEvento() {
-        return this.evento;
-    }
-
-    public void evoluir() {
-        evento = gerarEvento();
-        mostrar();
-    }
-
-    private Evento gerarEvento() {
+    public Ambiente() {
+        /**
+         * Utilização de um HashMap para não ser utilizado repetitivamente
+         * if's e switch-cases, fazendo diretamente a ligação entre o input
+         * e os eventos.
+         */
+        eventos = new HashMap<>();
         eventos.put("S", Evento.SILENCIO);
         eventos.put("R", Evento.RUIDO);
         eventos.put("A", Evento.ANIMAL);
         eventos.put("FU", Evento.FUGA);
         eventos.put("FO", Evento.FOTOGRAFIA);
         eventos.put("T", Evento.TERMINAR);
-        return null;
+    }
+    /**
+     * Função que retorna o valor do atributo privado evento.
+     *
+     * @return Evento atual no ambiente.
+     */
+    public Evento getEvento() {
+        return this.evento;
     }
 
-    private void mostrar() {
+    /**
+     * Método encarregado da evolução do ambiente permitindo mudar o evento que está a decorrer,
+     * chamando a seguir um método que permite a visualização disto ao utilizador.
+     */
+    public void evoluir() {
+        evento = gerarEvento();
+        mostrar();
+    }
 
+    /**
+     * Método para fazer a criação do próximo evento.
+     *
+     * @return novo Evento criado.
+     */
+    private Evento gerarEvento() {
+        System.out.println("Evento? ");
+        String com = sc.next();
+        return eventos.get(com);
+    }
+
+    /**
+     * Método que permite a visualização do evento atual na consola, para permitir
+     * uma melhor visibilidade dos acontecimentos do ambiente.
+     */
+    private void mostrar() {
+        if(evento != null) System.out.printf("Evento: %s\n", evento);
     }
 }
