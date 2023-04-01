440637e3fca7501839e464c88666edadda5ea5d9
tomas <A48286@alunos.isel.pt>
2023-04-01 15:42:13 +0100

    Todas as classes comentadas

--- iasa_agente/src/controlo_react/reaccoes/evitar/evitar_obst.py
+++ iasa_agente/src/controlo_react/reaccoes/evitar/evitar_obst.py
@@ -4,11 +4,14 @@ from ecr.hierarquia import Hierarquia
 from sae.ambiente.direccao import Direccao
 
 """
-    Classe EvitarObst que permite ao agente evitar obstáculos
+    Classe EvitarObst que representa um comportamento que permite ao agente evitar obstáculos, sendo estes as paredes do jogo.
+    A colisão pode ser o agente estar em contacto com a parede ou tentar mover-se em contra a parede, sendo assim, o objetivo
+    é que durante as exploração, o agente não colida com os obstáculos.
 """
 class EvitarObst(Hierarquia):
     """
-        Construtor da classe EvitarObst
+        Construtor da classe EvitarObst onde é criada a resposta apartir da classe RespostaEvitar() e depois, utilizando essa resposta
+        são criadas 4 instâncias de EvitarDir(), uma para cada direção.
     """
     def __init__(self):
         self.__resposta = RespostaEvitar()
